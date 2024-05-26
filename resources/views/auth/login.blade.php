8127381a1c2c6ede5dba6d06fff06c7ac2c5f869
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-05-26 19:19:23 +0700

    login dan dashboard style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,29 +2,115 @@
 <html>
 <head>
     <title>Login Karyawan</title>
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <style>
+        body {
+            font-family: "Inter";
+            background-color: #f0f0f0;
+            background-image: url("https://res.cloudinary.com/dq11ayv02/image/upload/v1716716656/project-pemweb/bvq3ur5ssuk8dz74r26m.png");
+        }
+        h1{
+            color: #f0f0f0;
+            align-self: center;
+        }
+        .navbar {
+            color: #fff;
+            padding: 10px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .navbar img {
+            height: 40px;
+            margin-right: 10px;
+        }
+        .container {
+            justify-content: center;
+            display: flex;
+            flex-direction: column;
+            max-width: 500px;
+            margin: 20vh 33vw;
+            padding: 20px;
+            background-color: transparent;
+        }
+        input{
+            border-radius: 27px;
+        }
+        .form-group input::placeholder{
+            font-size: small;
+            color: #fff;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 0 5px;
+        }
+        .form-group {
+            margin-bottom: 20px;
+            justify-content: center;
+            display: flex;
+            color: red;
+        }
+        .form-group label {
+            display: block;
+            margin-bottom: 10px;
+        }
+        .form-group input[type="text"], .form-group input[type="password"] {
+            color: #f0f0f0;
+            font-size: small;
+            width: 70%;
+            height: 25px;
+            padding: 10px;
+            border-radius: 15px;
+            border: none;
+            background: rgba(255, 255, 255, .1) 
+        }
+        .form-group button[type="submit"] {
+            background-color: #A41045;
+            font-size: medium;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 15px;
+            cursor: pointer;
+            display: inline-block;
+        }
+        .form-group button[type="submit"]:hover {
+            background-color: #444;
+        }
+        .logo{
+            align-self: center;
+            height: 103px;
+            width: 106px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Login Karyawan</h1>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <div>
-            <label for="nama">Nama Karyawan:</label>
-            <input type="text" id="nama" name="nama" value="{{ old('nama') }}" required>
-        </div>
-        <div>
-            <label for="id">ID Karyawan:</label>
-            <input type="password" id="id" name="id" value="{{ old('id') }}" required>
-            
-        </div>
-        @error('nama')
-                <div>{{ $message }}</div>
-        @enderror
-        @error('id')
-                <div>{{ $message }}</div>
+    <nav class="navbar">
+        <img src="https://res.cloudinary.com/dq11ayv02/image/upload/v1716717495/project-pemweb/wm1p31ly4jzn1xleizsp.png" alt="Logo">
+        <span class="fa fa-power-off"></span>
+    </nav>
+    <div class="container">
+        <img class="logo" src="https://res.cloudinary.com/dq11ayv02/image/upload/v1716725390/project-pemweb/logo.png" alt="Logo">
+        <h1>Login Karyawan</h1>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group">
+                <input type="text" id="nama" name="nama" placeholder="Username" value="{{ old('nama') }}" required>
+            </div>
+            <div class="form-group">
+                <input type="password" id="id" name="id" placeholder="Password" value="{{ old('id') }}" required>
+            </div>
+            @error('nama')
+                <div  class="form-group">{{ $message }}</div>
             @enderror
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
+            @error('id')
+                <div  class="form-group">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <button type="submit">Log In</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,17 @@
     <title>Dashboard</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
+            font-family: "Inter";
+            background-image: url("https://res.cloudinary.com/dq11ayv02/image/upload/v1716719875/project-pemweb/homeBG.png");
+            background-color: #333;
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #fff;
         }
         .container {
-            max-width: 800px;
-            margin: 40px auto;
-            padding: 20px;
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+           padding-top: 5vh;
+           max-width: 50vw;
+           padding-left: 7vw;
         }
         .header {
             background-color: #333;
@@ -20,25 +22,69 @@
             padding: 10px;
             text-align: center;
         }
-        .header h1 {
-            margin: 0;
+        h2 {   
+            font-size: 100px;
+            padding:0%;
+            font-weight: 900;
+            background: -webkit-linear-gradient(#eee, #333);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
         }
         .content {
             padding: 20px;
         }
+        .btn {
+            background-color: #A41045;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 27px;
+            text-decoration: none;
+            display: inline-block;
+        }
+        .navbar {
+            display: flex;
+            justify-content: center;
+            overflow: hidden;
+            padding: 10px;
+        }
+        .navbar a {
+            float: left;
+            color: #fff;
+            text-align: center;
+            padding: 10px 16px;
+            text-decoration: none;
+        }
+        .navbar a:hover {
+            color: #E52996;
+            opacity: 83%;
+        }
+        .logo {
+            height: 30px;
+            margin-right: 30vw;
+            float: left;
+            justify-self: start;
+        }
+        .fa{
+            margin-left: 30vw;
+        }
     </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
 <body>
+    <div class="navbar">
+        <img src="https://res.cloudinary.com/dq11ayv02/image/upload/v1716717495/project-pemweb/wm1p31ly4jzn1xleizsp.png" alt="Logo" class="logo">
+        <a href="/dashboard">Home</a>
+        <a href="#">Slip Gaji</a>
+        <a href="#">Profil Karyawan</a>
+        <a class="fa fa-power-off"></a>
+    </div>
     <div class="container">
-        <div class="header">
-            <h1>CMAS</h1>
-        </div>
         <div class="content">
             <h2>Selamat Datang, {{ $user->nama }}!</h2>
-            <p>Home | fours & travel</p>
-            <a href="#" class="btn">Lihat Slip</a>
             <p>Situs Slip Gaji PT. MAS Tours and Travel</p>
-            <p>IDN | Profil Karyawan</p>
+            <a href="#" class="btn">Lihat Slip</a>
         </div>
     </div>
 </body>
